5c8484efb0e54986b4950e73e723279009e23b5e
samuel martins <samuel.martins4.sm@gmail.com>
2022-06-01 17:45:33 +0300

    feature: create the signup-login user registration

---
+++ users/ forms.py
@@ -0,0 +1,10 @@
+# from django.forms import EmailField, forms
+# from django.contrib.auth.forms import UserCreationForm
+# from django.contrib.auth.models import User
+
+
+# class UserSignupForm(UserCreationForm):
+#     email = EmailField()
+
+#     class Meta:
+#         model = User
--- users/views.py
+++ users/views.py
@@ -1,6 +1,26 @@
-from django.shortcuts import render
+from pyexpat.errors import messages
+from django.shortcuts import render, redirect
+from django.http import HttpResponse
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib import messages
+
 
 # Create your views here.
 
 def login(request):
     return render(request, 'users/login.html')
+
+
+def signup(request):
+    if request.method == 'POST':
+        print('\n\n the code gets inside post check \n\n')
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            print('\n\n the code gets inside the if valid function \n\n')
+            username = form.cleaned_data.get('username')
+            # form.save()
+            messages.success(request, f"Thank you, {username}. Your accout has been successfuly created! Login to continue...")
+            return redirect('users_login')
+    else:
+        form = UserCreationForm()
+    return render(request, 'users/signup.html', {'form': form})
