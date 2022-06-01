5aeada45c3dc90bc606dec1cba8ae26713a31dd1
samuel martins <samuel.martins4.sm@gmail.com>
2022-06-01 14:55:18 +0300

    style: structure and style users signin-signup page

--- movies/views.py
+++ movies/views.py
@@ -4,7 +4,3 @@ from django.http import HttpResponse
 # Create your views here.
 def home(request):
     return render(request, 'movies/index.html')
-
-
-def another(request):
-    return HttpResponse('this is another page')
