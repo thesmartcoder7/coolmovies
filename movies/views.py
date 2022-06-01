6301738b805f5cf5964b03bf5e0aaa3fe8320d90
samuel martins <samuel.martins4.sm@gmail.com>
2022-06-01 13:48:18 +0300

    chore: initial django setup

--- movies/views.py
+++ movies/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
 # Create your views here.
+def home(request):
+    return render(request, 'movies/index.html')
+
+
+def another(request):
+    return HttpResponse('this is another page')
