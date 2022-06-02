b6394a5aa652e03841832fae04abcdc29e6e925d
samuel martins <samuel.martins4.sm@gmail.com>
2022-06-02 22:15:08 +0300

    chore: update media upload directory

--- users/models.py
+++ users/models.py
@@ -1,3 +1,11 @@
+from xml.etree.ElementInclude import default_loader
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+class Profile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    image = models.ImageField(default='default.jpg', upload_to='prifile_pics')
+
+    def __str__(self):
+        return f'{self.user.username} Profile'
