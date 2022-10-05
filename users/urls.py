8bb3a01a0a0b9040ebb2c405fe8cd2c95cb8eadd
thesmartcoder7 <samuel.martins4.sm@gmail.com>
2022-10-05 04:30:21 +0300

    patch: transfer repositiory a cloned one to new

--- users/urls.py
+++ users/urls.py
@@ -1,10 +1,9 @@
-from re import template
 from django.urls import path
 from django.contrib.auth import views as auth_views
 from . import views
 
 urlpatterns = [
-    path('signup', views.signup, name='users_signup'),
+    path('signup/', views.signup, name='users_signup'),
     path('login/', auth_views.LoginView.as_view(template_name='users/login.html'), name='users_login'),
     path('logout/', auth_views.LogoutView.as_view(template_name='movies/index.html'), name='users_logout'),
     path('update/', views.update, name='users_update')
