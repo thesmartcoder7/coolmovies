049deceb775f1ec8277bdd923d366961056a267d
thesmartcoder7 <samuel.martins4.sm@gmail.com>
2023-06-25 23:33:17 +0300

    docs: document the signals functions

--- users/signals.py
+++ users/signals.py
@@ -3,24 +3,38 @@ from django.contrib.auth.models import User
 from django.dispatch import receiver
 from .models import Profile
 
-"""
-post_save is a signal that gets fired when an object is saved
-in line 2, the User model that is being imported is what is going
-to be the sender since it is going to be sending the post_save signal
-
-the receiver being imported is what will be used to create the receiver
-funcition for receiving the signal sent from the User after a user
-has been saved successfully 
-
-"""
 
 @receiver(post_save, sender=User)
 def create_profile(sender, instance, created, **kwargs):
+    """
+    Signal receiver function to create a profile for a newly created user.
+
+    Args:
+        sender: The model class that sent the signal (User).
+        instance: The actual instance of the model (User instance).
+        created (bool): Flag indicating whether the user was created or not.
+        **kwargs: Additional keyword arguments.
+
+    Returns:
+        None
+
+    """
     if created:
         Profile.objects.create(user=instance)
 
 
-
 @receiver(post_save, sender=User)
 def save_profile(sender, instance, **kwargs):
+    """
+    Signal receiver function to save the profile when a user is saved.
+
+    Args:
+        sender: The model class that sent the signal (User).
+        instance: The actual instance of the model (User instance).
+        **kwargs: Additional keyword arguments.
+
+    Returns:
+        None
+
+    """
     instance.profile.save()
